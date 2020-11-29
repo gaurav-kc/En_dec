0b544298190cd7978a7643d86b269047ca4622f2
gaurav <gaurav.chaudhari@students.iiit.ac.in>
2020-11-29 12:29:09 +0530

    First commit

---
+++ decode.py
@@ -0,0 +1,40 @@
+with open("temp.lol", "rb") as image:
+    f = image.read()
+    b = bytearray(f)
+imgcount = bytearray()
+for i in range(0,4):
+    imgcount.append(b[i])
+
+imgcount = int.from_bytes(imgcount, byteorder='little')
+print(imgcount)
+index = 4
+sizes = []
+for i in range(0,imgcount):
+    count = 4
+    temp = bytearray()
+    while count:
+        count = count - 1
+        temp.append(b[index])
+        index = index + 1
+    temp = int.from_bytes(temp, byteorder='little')
+    print(temp)
+    sizes.append(temp)
+
+filecount = 0
+for i in range(0,imgcount):
+    filename = "file_" + str(filecount) + ".png"
+    currsize = sizes[filecount]
+    filecount = filecount + 1
+    tempimg = bytearray()
+    while currsize:
+        currsize = currsize - 1
+        b[index] = (b[index] + 200)%256
+        tempimg.append(b[index])
+        index = index + 1
+    f = open(filename,"wb")
+    f.write(tempimg)
+    f.close()
+# f = open("original.png","wb")
+# for i in range(0, len(b)):
+#     b[i] = (b[i] + 200)%256
+# f.write(b)
---
+++ encode.py
@@ -0,0 +1,29 @@
+# find recent code at 
+# first argument is the input directory name 
+# second argument is the output directory name 
+
+with open("Screenshot from 2020-11-28 15-55-25.png", "rb") as image:
+    f1 = image.read()
+    b1 = bytearray(f1)
+with open("Screenshot from 2020-11-28 15-56-22.png", "rb") as image:
+    f2 = image.read()
+    b2 = bytearray(f2)
+
+imgcount = 2 
+len1 = int(len(b1))
+len2 = int(len(b2))
+print(len1)
+print(len2)
+len1 = len1.to_bytes(4,'little')
+len2 = len2.to_bytes(4,'little')
+imgcount = imgcount.to_bytes(4,'little')
+
+f = open("temp.lol","wb")
+
+for i in range(0, len(b1)):
+    b1[i] = (b1[i] + 56)%256
+for i in range(0, len(b2)):
+    b2[i] = (b2[i] + 56)%256
+
+final = imgcount + len1 + len2 + b1 + b2
+f.write(final)
