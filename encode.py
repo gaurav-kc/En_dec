5f4d4b1abe156a7c4aaca228381fb3fd7c2fa93d
gaurav <gaurav.chaudhari@students.iiit.ac.in>
2020-11-29 12:50:32 +0530

    added 222

--- encode.py
+++ encode.py
@@ -1,7 +1,7 @@
 # find recent code at 
 # first argument is the input directory name 
 # second argument is the output directory name 
-# adding a comment 
+# adding a comment 222
 # comment from git
 with open("Screenshot from 2020-11-28 15-55-25.png", "rb") as image:
     f1 = image.read()
