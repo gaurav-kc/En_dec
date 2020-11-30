1baf9e7b25c59ab05f19bb0dee22da15c7062f58
gaurav <gaurav.chaudhari@students.iiit.ac.in>
2020-11-30 09:43:55 +0530

    comments added and existing file check

--- decode.py
+++ decode.py
@@ -1,7 +1,7 @@
 # find this project at https://github.com/gaurav-kc/Enc_dec
 
-# First argument is mandatory. Rest 3 are are optional. But order is important 
-# first argument is the input directory name. It should have all the chunks
+# first argument is the input directory name. It should have all the chunks 
+# (it is okay if it has other files. The program reads only recognized format files)
 # second argument is the output directory name. It will have the files
 # third argument is the chunksize. If you mentioned it while encrypting
 # fourth argument is key (0-255) only . If mentioned while encrypting
@@ -10,6 +10,7 @@ import sys
 import os
 import json
 
+# default values
 directory_name = "encrypted" #1st argument
 op_directory_name = "decrypted" #2nd argument
 chunksize = 10000 #3rd argument
@@ -17,7 +18,7 @@ key = 200 #4th argument
 commonname = "bpsnecjkx"
 delimeter = "_"
 opformat = "gty"
-# allowed_formats = ["jpg","png","jpeg"]
+# allowed_formats = ["jpg","png","jpeg"] # of no use here
 endian = 'little'
 
 if len(sys.argv) >= 2:
@@ -35,12 +36,15 @@ def decrpyt_byte(by,key):
     by = (by + key)%256
     return by
 
-# let us construct the entire object
+# construct the entire bytearray (combined of all chunks)
+# check if given directory exists
 dir_path = "./" + directory_name + "/"
 if not os.path.exists(dir_path):
     print("Directory not found")
     exit(0)
 
+# this step is to avoid all other files apart from what we recognize as chunk
+# we only take count of such files and assume the files are from 0 to filecount-1
 all_files = os.listdir(dir_path)
 filecount = 0
 for afile in all_files:
@@ -49,36 +53,45 @@ for afile in all_files:
     if format == opformat:
         filecount = filecount + 1
 
-print(filecount)
+print(filecount)    # debug purpose only 
 if filecount == 0:
     print("No compatible files found")
     exit(0)
 
+# finalres will have the entire bytes 
 finalres = bytearray()
 for i in range(0,filecount):
+    # i here is chunk number
     filename = dir_path + commonname + delimeter + str(i) + "." + opformat
     if not os.path.exists(filename):
         print(filename, " not found")
-        exit(0)
-    f = (open(filename,"rb")).read()
+        exit(0) # bilkul ricks nai lene ka
+    file_obj = open(filename,"rb")
+    f = file_obj.read()
     b = bytearray(f)
-    finalres = finalres + b
-print(len(finalres))
+    finalres = finalres + b     # append bytearray of current file to finalres
+    file_obj.close() 
+
+print(len(finalres))  # debug only
 
 #entire object I have now
-index = 0
+index = 0   # this index will be our pointer to finalres. 
+# in case any data is read through finalres, never ever forget to inc index 
 
-opfilecount = bytearray()
-for i in range(0,4):
+opfilecount = bytearray()   # this will have number of files that will be created
+for i in range(0,4):    # in our case, first 4 bytes will be interpreted as integer and stores how many files are there
     opfilecount.append(finalres[index])
     index = index + 1
 opfilecount = int.from_bytes(opfilecount, byteorder=endian)
-print(opfilecount)
+print(opfilecount)  # debug only
 
+# now we know files count 
+# we know our format is bytecount bytes bytecount bytes ... 
+# so we read that many bytes (each such bytes will be one decrypted file)
 bytes_list = []
 for i in range(opfilecount):
     filesize = bytearray()
-    for j in range(0,4):
+    for j in range(0,4):    # 4 bytes to store bytes count 
         filesize.append(finalres[index])
         index = index + 1
     filesize = int.from_bytes(filesize, byteorder=endian)
@@ -86,23 +99,36 @@ for i in range(opfilecount):
     for j in range(0,filesize):
         temp.append(decrpyt_byte(finalres[index],key))
         index = index + 1
-    bytes_list.append(temp)
+    bytes_list.append(temp) # read filesize bytes into seperate bytearray and add it to the list
 
-actual_names = bytearray()
+# after storing all bytes, we had encoded and appended semi-colon seperated list of original filenames
+# we recover it now
+actual_names = bytearray()  
 while index < len(finalres):
     actual_names.append(finalres[index])
     index = index + 1
-actual_names = actual_names.decode("utf-8") 
+actual_names = actual_names.decode("utf-8") #decode that bytearray into string
 actual_names = actual_names.split(";")
-actual_names.pop()
-print(actual_names)
+actual_names.pop() # last element will be empty as format was filename1;filename2; .. filenamek;
+print(actual_names) #debug only
 
+# now we have bytearray and filename in 2 lists. We create a file with filename and write the bytearray to it
+# In encrypted format, the order of bytes and filenames was same
+
+# check if output directory exists
 opdir_path = "./" + op_directory_name + "/"
 if not os.path.exists(opdir_path):
     os.mkdir(opdir_path)
 
 for i in range(0,opfilecount):
     rec_filename = opdir_path + actual_names[i]
+    # check if file exists 
+    if os.path.exists(rec_filename):
+        print("The file ",actual_names[i], " already exists. Overwrite it? y/n")
+        choice = input()
+        if choice != "y":
+            continue
     f = open(rec_filename, "wb")
     f.write(bytes_list[i])
+    f.close()
 print("Done")
--- encode.py
+++ encode.py
@@ -1,6 +1,5 @@
 # find this project at https://github.com/gaurav-kc/Enc_dec
 
-# First argument is mandatory. Rest 3 are are optional. But order is important 
 # first argument is the input directory name. It should have all the
 # files you wish to encrypt.
 # second argument is the output directory name. It will have the chunks
@@ -11,6 +10,7 @@ import sys
 import os
 import json
 
+#default values 
 directory_name = "testit" #1st argument
 op_directory_name = "encrypted" #2nd argument
 chunksize = 10000 #3rd argument
@@ -37,23 +37,22 @@ def encrpyt_byte(by,key):
     return by
 
 rel_pathname = "./" + directory_name + "/"
-#check directory name is correct or not 
+#check input directory name is correct or not 
 isdirectory = os.path.isdir(rel_pathname)
 if not isdirectory:
     print("Directory not found")
     exit(-1)
 
 #check all files and formats
-
 tfile_names = os.listdir(rel_pathname)
-file_names = []
+file_names = [] #this will have all filenames which will be actually encrypted
 filenames_enc = str()
 #analyze these names of files in directory
 for tfile in tfile_names:
     temp = tfile.split(".")
     #only one dot is allowed in filename
     if len(temp) != 2:
-        print("Incorrect format ",tfile)
+        print("Incorrect format ",tfile, " skipping it.")
         continue
     filename = temp[0]
     format = temp[1]
@@ -64,6 +63,8 @@ for tfile in tfile_names:
     file_names.append(tfile)
     filenames_enc = filenames_enc + tfile + ";"
 
+# this was added to restore the original filenames 
+# it is of type filename1;filename2; .. filenamek;
 filenames_enc = bytearray(filenames_enc,'utf-8')
 
 files_count = len(file_names)
@@ -71,7 +72,7 @@ if files_count == 0:
     print("Nothing to encrpyt")
     exit(0)
 
-finalres = bytearray()
+finalres = bytearray()  # this will have the entire bytearray
 files_count = files_count.to_bytes(4, endian)   #conv to bytearray
 finalres = finalres + files_count #first 4 bytes for file count 
 
@@ -87,21 +88,22 @@ for tfile in file_names:
             b[i] = encrpyt_byte(b[i],key)
         #append the data
         size = size.to_bytes(4,endian)  #if file size goes beyound 4GB handle here
-        finalres = finalres + size + b
-#we convert the list and append at the end to restore the 
-#original names 
+        finalres = finalres + size + b  #append byte count and that many bytes
+
+# we convert the list and append at the end to restore the original names 
 finalres = finalres + filenames_enc
-print("finalres is ",len(finalres))
-#now we have all bytes in finalres variable.
-#we need to chunk it
-#a warning as wrong chunk size can create millions of chunks
+print("finalres is ",len(finalres)) #this is for debugging
+
+# now we have all bytes in finalres variable.
+# we need to chunk it
+# a warning is given as wrong chunk size can create millions of chunks
 chunkcount = int(len(finalres)/chunksize) + 1 
 print(chunkcount, " chunks will form. Are you sure you want to continue? y/n")
 choice = input()
 if choice != "y":
     exit(0)
 
-chunk_array = []
+chunk_array = []    # this will be array of bytearrays where each bytearray is a chunk
 i = 0
 while i < len(finalres):
     chunk = bytearray()
@@ -113,13 +115,17 @@ while i < len(finalres):
     chunk_array.append(chunk)
 
 #now we have byte chunks
-#give crazy file names 
+
 output_direc = "./" + op_directory_name
-os.mkdir(output_direc)
+# check if directory exists
+if not os.path.exists(output_direc):
+    os.mkdir(output_direc)
+
+# in that dir, put all files
 for i in range(0,len(chunk_array)):
     #i will be the chunk number
     chunkname = commonname + delimeter + str(i) + "." + opformat
     chunkname = os.path.join(output_direc,chunkname)
     f = open(chunkname,"wb")
     f.write(chunk_array[i])
-    f.close()   
+    f.close()       # this is important :D
